ce52200f05e802d93102f26b5be0d876ffa0f950
Akhila5diddi <akhila5netha@gmail.com>
2020-02-23 23:13:09 -0700

    commit final
    final commit

---
+++ Calculator/Assets/ArthematicOperations.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class ArthematicOperations : MonoBehaviour
+{
+     public InputField Field1;
+     public InputField Field2;
+     public InputField Result;
+ 
+     public void Sum() {
+         int a = Convert.ToInt32(Field1.text);
+         int b = Convert.ToInt32(Field2.text);
+         int c = a+b;
+         Result.text = c.ToString();
+     }
+ 
+ 
+     public void Product() {
+         int a = Convert.ToInt32(Field1.text);
+         int b = Convert.ToInt32(Field2.text);
+         int c = a*b;
+         Result.text = c.ToString();
+     }
+
+	  public void Division() {
+         int a = Convert.ToInt32(Field1.text);
+         int b = Convert.ToInt32(Field2.text);
+         int c = a/b;
+         Result.text = c.ToString();
+     }
+	 public void Subtraction() {
+         int a = Convert.ToInt32(Field1.text);
+         int b = Convert.ToInt32(Field2.text);
+         int c = a-b;
+         Result.text = c.ToString();
+     }
+}
